775e7f0dc1d04b5be34a4501df9964d3d53f08bf
snct-ukai <73654679+snct-ukai@users.noreply.github.com>
2021-02-20 21:24:39 +0900

    プロジェクト ファイルを追加します。

---
+++ switch exp/MainActivity.cs
@@ -0,0 +1,47 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using System;
+
+namespace switch_exp
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        public TextView text { get; private set; }
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            text = FindViewById<TextView>(Resource.Id.text);
+            Switch test = FindViewById<Switch>(Resource.Id.test);
+            test.Click += Test_Click;
+        }
+
+        private void Test_Click(object sender, EventArgs e)
+        {
+            Switch test = FindViewById<Switch>(Resource.Id.test);
+            if(test.Checked == true)
+            {
+                text.Text = "On";
+            }
+            else
+            {
+                text.Text = "Off";
+            }
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
